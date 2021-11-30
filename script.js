600d0c9a5cd03424ee51dda6de7bf6843f3295d5
Sivani Saravanan <31069721+inavis@users.noreply.github.com>
2021-11-30 23:50:36 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,80 +1,126 @@
 
-//create like enter fully-qualified domain name such as wikipedia.org, youtube.com, or bbc.co.uk.
-//Sometimes, a website doesn't have any icons at all – or that site might not exist. But in your UI, you may want to still show an icon anyway, and a broken image doesn't look too nice.
-//For this reason, we automatically serve a fallback icon
 
-//<img src="https://icon.horse/icon/youtube.com"> -->just load image like this
-//display like 10 to compare and the search term
+let tab1 = document.querySelector(".tab1")
+let tab2 = document.querySelector(".tab2")
+let sect= document.querySelector(".sect")
+let chap= document.querySelector(".chap")
+let chapgrp = document.querySelector(".chapgrp")
+let lines = document.querySelector(".lines")
+let explanation = document.querySelector(".explanation")
+let head = document.querySelector(".head")
+let head1 = document.querySelector(".head1")
+let head2 = document.querySelector(".head2")
+let head3 = document.querySelector(".head3")
+let head4 = document.querySelector(".head4")
+let head5 = document.querySelector(".head5")
+let error = document.querySelector(".error")
+let searchbar = document.querySelector("#searchbar")
 
+let kural=1;
+let language="tamil";//initially tamil content is diaplayed
 
-let searchbar = document.querySelector("#searchbar");
-let content = document.querySelector(".content");
-let value;
+function getData(value){
+    //alert(value)
+    
+    changestyle(value);
+    api(kural);
 
+}
+function changestyle(value){
+   // alert(value)
+   if(value=="tamil"){
+       language="tamil";
+       tab1.style.color="rgb(143, 34, 34)"
+       tab1.style.borderBottom="5px solid rgb(143, 34, 34)"
+       tab1.style.backgroundColor="rgb(214, 213, 213)"
+    //    tab1.style.backgroundColor="rgb(236, 169, 169)";
+
+    tab2.style.color="gray";
+    tab2.style.borderBottom="1px solid black";
+    tab2.style.backgroundColor="white";
+   }else if(value=="english"){
+       language="english";
+    tab2.style.color="rgb(235, 47, 235)";
+    tab2.style.borderBottom="5px solid rgb(235, 47, 235)";
+    tab2.style.backgroundColor="rgb(214, 213, 213)";
+    // tab2.style.backgroundColor="rgb(199, 175, 230)";
+
+    tab1.style.color="gray";
+    tab1.style.borderBottom="1px solid black";
+    tab1.style.backgroundColor="white";
+   }
+}
 
 function search(){
-     value = searchbar.value.trim();
-        if(value!=""){
-            //removes http or https if present
-            //if https://github.com -> github.com
-            //if https://www.w3schools.com -> www.w3schools.com
-            if(value.includes("https://")==true ){
-                value=value.split("https://")[1]
-             } 
-              else if(value.includes("http://")==true){
-                 value=value.split("http://")[1]
-                 console.log(value)
-                 }
-                 //removes if any extra parameters ,only till xyz.com/...
-                 // if enter "https://github.com/inavis/get-thirukural" -> gives github logo
-                 // if https://www.w3schools.com/jsref/jsref_includes.asp -> w3schools logo
-                if(value.includes("/")){
-                    value=value.split("/")[0];
-                } 
-             
-        // console.log(value)
-         api(value)
-        }else{
-            appear("Enter any domain name to search.It cannot be empty")
-            // alert("cannot be empty")
-        }
+    if(searchbar.value>0 && searchbar.value<=1330){
+        error.style.display="none"
+        kural = Number(searchbar.value);
+       // console.log(kural)
+        api(kural);
+    }else{
+        error.innerHTML="ERROR : ONLY FROM 1 TO 1330"
+        error.style.display="block"
+    }
+    //console.log(kural);
     
 }
 
-function api(value){
-    let url5 =`https://icon.horse/icon/${value}`
-fetch(url5)
-.then(function(res){
-   // console.log(res.url)
-   let div = document.createElement("div");
-   div.classList="card m-5";
-   content.appendChild(div)
-   let div1 = document.createElement("div");
-   div1.className="title"
-   div1.innerHTML=value;
-   div.appendChild(div1)
-   let div2 = document.createElement("div");
-   div2.className="icon";
-   let img = document.createElement("img");
-   img.setAttribute("src",res.url);
-   img.setAttribute("height","200px")
-   img.setAttribute("width","200px")
-   div2.appendChild(img)
-   div.appendChild(div2);
-
-   searchbar.value=""
-})
-.catch(function(err){
-    console.log(err);
-    appear("Please check the entered URL again or failed to  load the resource");
-    // alert("Please check the entered URL again or failed to  load the resource")
-})
+function api(number){
+    //console.log("number",number)
+    let url=`https://api-thirukkural.vercel.app/api?num=${number}`;
+    fetch(url)
+    .then((res)=>res.json())
+    .then(function(data){
+        if(language=="english"){
+            //console.log("kural in english");
+            head.innerHTML="Thirukural";
+            head1.innerHTML="Section"+" : ";
+            head2.innerHTML="Chapter"+" : ";
+            head3.innerHTML="Chapter group"+" : ";
+            head4.innerHTML="Lines"+" : ";
+            head5.innerHTML="Explanation"+" : ";
+            sect.innerHTML=data['sect_eng']
+            chap.innerHTML=data['chap_eng']
+            chapgrp.innerHTML=data['chapgrp_eng']
+            lines.innerHTML=data['eng']
+            explanation.innerHTML=data['eng_exp']
+           
+        }else{
+            //console.log("kural in tamil");
+            head.innerHTML="திருக்குறள் தெளிவுரை";
+            head1.innerHTML="பால்"+" : ";
+            head2.innerHTML="அதிகாரம்"+" : ";
+            head3.innerHTML="அதிகாரம் வகை"+" : ";
+            head4.innerHTML="குறள்"+" : ";
+            head5.innerHTML="விளக்கம்"+" : ";
+            sect.innerHTML=data['sect_tam']
+            chap.innerHTML=data['chap_tam']
+            chapgrp.innerHTML=data['chapgrp_tam']
+            lines.innerHTML=`${data['line1']} <br> ${data['line2']}`
+            explanation.innerHTML=data['tam_exp']
+               }
+    })
+    .catch((err)=>console.log(err));
 }
 
-function appear(value){
-    document.querySelector(".message").innerHTML=value;
-    document.getElementById("confirm").style.display="block";
+api(kural); // run once to display first kural when page loads
+
+function prev(){
+    if(kural<1){
+       
+    }else{
+        kural-=1;
+        //console.log(kural);
+        api(kural)
+    }
 }
-function disappear(){
-    document.getElementById("confirm").style.display="none";
+
+function next(){
+    if(kural>1330){
+
+    }else{
+        kural+=1;
+        //console.log(kural)
+        api(kural);
+    }
 }
